579019397ff3b2f0f947c81bc26afb70c53e734c
Richard <richardlwhittington@gmail.com>
2024-08-10 12:09:08 -0500

    test on new site

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
 import puppeteer from "puppeteer";
 
-const getQuotes = async () => {
-  // Start a Puppeteer session with:
-  // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
-  // - no default viewport (`defaultViewport: null` - website page will be in full width and height)
+ const getTexts = async () => {
+//   // Start a Puppeteer session with:
+//   // - a visible browser (`headless: false` - easier to debug because you'll see the browser in action)
+//   // - no default viewport (`defaultViewport: null` - website page will be in full width and height)
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: null,
   });
 
-  // Open a new page
-  const page = await browser.newPage();
+//   // Open a new page
+   const page = await browser.newPage();
 
-  // On this new page:
-  // - open the "http://quotes.toscrape.com/" website
-  // - wait until the dom content is loaded (HTML is ready)
-  await page.goto("http://quotes.toscrape.com/", {
+//   // On this new page:
+//   // - open the "http://quotes.toscrape.com/" website
+//   // - wait until the dom content is loaded (HTML is ready)
+   await page.goto("https://scotlandtrip2022.netlify.app/", {
     waitUntil: "domcontentloaded",
   });
 
-  // Get page data
-  const quotes = await page.evaluate(() => {
-    // Fetch the first element with class "quote"
-    // Get the displayed text and returns it
-    const quoteList = document.querySelectorAll(".quote");
+//   // Get page data
+  const texts = await page.evaluate(() => {
+//     // Fetch the first element with class "quote"
+//     // Get the displayed text and returns it
+   const textList = document.querySelectorAll(".card-body");
 
-    // Convert the quoteList to an iterable array
-    // For each quote fetch the text and author
-    return Array.from(quoteList).map((quote) => {
-      // Fetch the sub-elements from the previously fetched quote element
-      // Get the displayed text and return it (`.innerText`)
-      const text = quote.querySelector(".text").innerText;
-      const author = quote.querySelector(".author").innerText;
+//     // Convert the quoteList to an iterable array
+//     // For each quote fetch the text and author
+    return Array.from(textList).map((text) => {
+//       // Fetch the sub-elements from the previously fetched quote element
+//       // Get the displayed text and return it (`.innerText`)
+     //const fullName = staff.querySelector(".fullname").innerText;
+      const label = text.querySelector(".card-text").innerText;
 
-      return { text, author };
+     return { label };
     });
   });
 
-  // Display the quotes
-  console.log(quotes);
+//   // Display the quotes
+   console.log(texts);
 
-    // Click on the "Next page" button
-    //await page.click(".pager > .next > a");
-  // Close the browser
+//     // Click on the "Next page" button
+//     //await page.click(".pager > .next > a");
+//   // Close the browser
   await browser.close();
-};
+ };
 
-// Start the scraping
-getQuotes();
+// // Start the scraping
+ getTexts();
